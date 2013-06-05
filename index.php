ab89aab7f89f0b49e9cc22c44d50a8a12f3ab1a2
db0company <db0company@gmail.com>
2013-06-05 12:25:47 +0200

    New team

--- index.php
+++ index.php
@@ -5,90 +5,6 @@
 // Latest Version is on GitHub: https://github.com/ReturnToLife/Dev           //
 // ************************************************************************** //
 
-function	progress() {
-  // do not execute unless needed
-  if ($_GET['p'] != 'progress')
-    return '';
-
-  // conf
-  $dateformat = 'M j';
-  $firstweek = 5;
-  $totalweek = 19;
-  $doclist = 'https://docs.google.com/spreadsheet/pub?key=0Ag8n0yHMUHF-dEVTd2FwazBYb29TT2JhdXF6NWFsSmc&single=true&gid=0&output=csv';
-
-  // get document links list
-  $f = fopen($doclist, 'r');
-  while ($l = fgetcsv($f)) {
-    $doclinks[intval($l[0])] = $l[1];
-  }
-  fclose($f);
-
-  // calculate dates
-  $menu = array();
-  for ($nbweek = $firstweek; $nbweek <= $totalweek; $nbweek++) {
-    $weekstart = strtotime('2013W'.($nbweek < 10 ? '0' : '').$nbweek);
-    $menu[$nbweek] =
-      array('start' => date($dateformat, $weekstart),
-	    'end'   => date($dateformat, strtotime('+ 6 days', $weekstart)));
-  }
-
-  // which week will be displayed?
-  $doc = intval(date('W')) - 1;
-  if (isset($_GET['w']))
-    $doc = $_GET['w'];
-  if (!array_key_exists(intval($doc), $menu))
-    $doc = $firstweek;
-
-  $ret = '      <div class="row-fluid">
-        <div class="span3">
-          <ul class="nav nav-tabs nav-stacked">
-';
-
-  foreach ($menu as $nbweek => $week) {
-    $ret .= '              <li';
-    if ($doc == $nbweek)
-      $ret .= ' class="active"';
-    $ret .= '>';
-    $ret .= '<a href="?p=progress&w='.$nbweek.'">';
-    $ret .= $week['start'].' - '.$week['end'];
-    $ret .= '</a></li>
-';
-  }
-
-  $ret .= '          </ul>
-        </div>
-       <div class="span9">
-         ';
-
-  $available_formats =
-    array('Excel'   => 'xls',
-	  'CSV'     => 'csv',
-	  'Text'    => 'txt',
-	  'PDF'     => 'pdf',
-	  'OpenDoc' => 'ods');
-  if (!array_key_exists(intval($doc), $doclinks))
-    $ret .= '<p>Not available.</p>
-';
-  else {
-    $ret .= '<i class="icon-download"></i> Download this file 
-';
-    foreach ($available_formats as $format => $extension) {
-      $ret .= '         - <a target="_blank" href="';
-      $ret .= str_replace('output=html', 'output='.$extension, $doclinks[$doc]);
-      $ret .= '">'.$format.'</a>
-';
-    }
-    $ret .= '         <hr>
-         <iframe class="smalliframe" src="'.$doclinks[$doc].'"></iframe>
-';
-  }
-
-  $ret .= '       </div>
-';
-
-  return $ret;
-  }
-
 $pages = array('home'     => array('name' => 'Presentation',
                                    'icon' => 'home',
                                    'content' => 'https://docs.google.com/document/d/1yTeGzE57fRWVcqIVqUHC72L_nBz-WHHvN-ZgDf43qpA/pub?embedded=true',
@@ -125,38 +41,22 @@ $pages = array('home'     => array('name' => 'Presentation',
                                    'icon' => 'share',
                                    'content' => 'https://docs.google.com/document/d/1H7FrCQJen_pwQSjZ93bPfwQo9umWkrQCDS2dsN8TyDQ/pub?embedded=true',
                                    'iframe' => true),
-               'progress' => array('name' => 'Progress',
-                                   'icon' => 'tasks',
-                                   'content' => progress(),
-                                   'iframe' => false),
                'team'     => array('name' => 'Team',
                                    'icon' => 'user',
                                    'content' => '      <div class="row-fluid">
-	<div class="span3 team">
+	<div class="span6 team">
 	  <img class="img-circle" width="150"  src="img/team/lepage_b.jpg" />
 	</div>
-	<div class="span3 team">
-	  <img class="img-circle" width="150"  src="img/team/bideau_s.jpg" />
-	</div>
-	<div class="span3 team">
-	  <img class="img-circle" width="150"  src="img/team/munoz_v.jpg" />
-	</div>
-	<div class="span3 team">
-	  <img class="img-circle" width="150"  src="img/team/meilha_v.jpg" />
+	<div class="span6 team">
+	  <img class="img-circle" width="150"  src="img/team/corfa_u.jpg" />
 	</div>
       </div>
       <div class="row-fluid">
-	<div class="span3 team">
+	<div class="span6 team">
 	  Barbara Lepage
 	</div>
-	<div class="span3 team">
-	  Sofia Bideaux
-	</div>
-	<div class="span3 team">
-	  Vincent Munoz
-	</div>
-	<div class="span3 team">
-	  Vivien Meilhac
+	<div class="span6 team">
+	  Uriel Corfa
 	</div>
       </div>',
                                    'iframe' => false),
